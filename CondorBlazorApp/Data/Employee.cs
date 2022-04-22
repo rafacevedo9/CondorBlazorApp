e281bf114ab0247cf138aa0bdbf9cc31e79389d5
Rafael Acevedo <rafacevedo9@gmail.com>
2022-04-21 22:50:13 -0500

    Agregar archivos de proyecto.

---
+++ CondorBlazorApp/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace CondorBlazorApp.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string TypeDocument { get; set; } = null!;
+        public string DocumentNumber { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateTime DateBirth { get; set; }
+        public string Area { get; set; } = null!;
+    }
+}
---
+++ CondorBlazorApp/Data/Respuesta.cs
@@ -0,0 +1,9 @@
+﻿namespace CondorBlazorApp.Data
+{
+    public class Respuesta<T>
+    {
+        public int Success { get; set; }
+        public string Message { get; set; } = null!;
+        public T Data { get; set; }
+    }
+}
